1cb7dd76ac9843345ec9ff7b8d09963f870cf567
Olivertrain1221 <olivertrain1996@gmail.com>
2022-02-16 23:06:20 +0000

    Add new article app to the project

--- djangointernet/djangointernet/views.py
+++ djangointernet/djangointernet/views.py
@@ -1,15 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def homepage(request):
     """
     About function linking the view when url is hit
     """
-    return HttpResponse("homepage")
+    return render(request, "homepage.html")
 
 
 def about(request):
     """
     About function linking the view when url is hit
     """
-    return HttpResponse("About")
+    return render(request, "about.html")
