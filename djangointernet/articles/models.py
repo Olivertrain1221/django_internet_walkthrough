f644061774b823eb04b6991123ff41dd73d9fc5b
Olivertrain1221 <olivertrain1996@gmail.com>
2022-02-17 15:06:03 +0000

    Made a class/database for Articles

--- djangointernet/articles/models.py
+++ djangointernet/articles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField(max_length=500)
+    date = models.DateField(auto_now_add=True)
+    # add in thumbnail later
+    # also add in author later
